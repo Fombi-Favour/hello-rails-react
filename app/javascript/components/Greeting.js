443fcfb614afbacb581e07f5a70aeb3cd31bc03c
Fombi Favour <fombifavour@gmail.com>
2024-02-24 13:48:58 +0100

    Update Greeting.js

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchGreeting } from '../redux/greeting/greetingSlice'
 
 const Greeting = () => {
+
+    const dispatch = useDispatch();
+    const greet = useSelector((state) => state.greeting.message);
+    useEffect(() => {
+        if (!greet) {
+            dispatch(fetchGreeting())
+            .then(() => {
+                console.log('Success!!!')
+            }).catch(() => { console.log('Error!!!') });
+        }
+    }, [dispatch])
+
     return (
-        <h1>Hello react rails</h1>
+        <h1> {greet} </h1>
     )
 }
 
